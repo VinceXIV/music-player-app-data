75f5a30efb491b17242f2c34188065a9220e8049
Vincent Makokha <vincent.makokha@student.moringaschool.com>
2022-10-05 19:06:08 +0300

    Add user database

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const comments = require("./data/music-comments");
 const musicInfo = require("./data/music-info");
+const users = require("../data/available-users")
 
 module.exports = {
     comments,
-    musicInfo
+    musicInfo,
+    users
 }
